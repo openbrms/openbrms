81fc190edc100e6e2104987af4548191006ff17e
Alex Kuleshov <alex@kuleshov.tech>
2023-03-31 19:51:23 -0400

    fixed tests

--- api/src/test/java/tech/kuleshov/ruleengine/api/ApiApplicationTests.java
+++ api/src/test/java/tech/kuleshov/ruleengine/api/ApiApplicationTests.java
@@ -1,9 +1,14 @@
 package tech.kuleshov.ruleengine.api;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @SpringBootTest
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+@Import(TestConfiguration.class)
 class ApiApplicationTests {
 
   @Test
