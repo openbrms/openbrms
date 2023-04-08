c2578705d03e4281aa6900c0fcfc38497ff0b62e
Alex Kuleshov <alex@kuleshov.tech>
2023-04-08 18:09:17 -0400

    cors config updated

--- api/src/main/java/tech/kuleshov/ruleengine/api/config/CorsConfig.java
+++ api/src/main/java/tech/kuleshov/ruleengine/api/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("*");
+                registry.addMapping("/**").allowedMethods("*").allowedOrigins("*");
             }
         };
     }
